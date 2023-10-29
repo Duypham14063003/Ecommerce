1c4ce04331a3ff1c2cea2df20dae2e3df3148eb4
Minh Huynh <minh932004@gmail.com>
2023-10-29 22:56:08 +0700

    first commit

---
+++ Ecommerce/Controllers/UsersController.cs
@@ -0,0 +1,84 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Controllers
+{
+    public class UsersController : Controller
+    {
+        private DoAnEntities1 database = new DoAnEntities1();
+        // GET: Users
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                if (string.IsNullOrEmpty(cust.NameCus))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(cust.PassCus))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (string.IsNullOrEmpty(cust.EmailCus))
+                    ModelState.AddModelError(string.Empty, "Email không được để trống");
+                if (string.IsNullOrEmpty(cust.PhoneCus))
+                    ModelState.AddModelError(string.Empty, "Điện thoại không được để trống");
+                var khachhang = database.Customers.FirstOrDefault(k => k.NameCus == cust.NameCus);
+                if (khachhang != null)
+                    ViewBag.Thongbao= "Đã có người  đăng kí tên này";
+                if (ModelState.IsValid)
+                {
+                    database.Customers.Add(cust);
+                    database.SaveChanges();
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("Login","Users");
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(cust.NameCus))
+                    ModelState.AddModelError(string.Empty, "Ten dang nhap nay khong duoc de trong");
+                if (string.IsNullOrEmpty(cust.PassCus))
+                    ModelState.AddModelError(string.Empty, "Mat khau khong duoc de trong");
+                if (ModelState.IsValid)
+                {
+                    var khachhang = database.Customers.
+                        FirstOrDefault(k => k.NameCus == cust.NameCus && k.PassCus == cust.PassCus);
+                    if (khachhang != null)
+                    {   
+                        Session["Taikhoan"] = khachhang.NameCus;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                        ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng! Vui lòng kiểm tra lại";
+                }
+
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
