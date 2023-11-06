11b58e874173bf217bd6e82f0835a923047d3fe3
chanwa123 <148076300+chanwa123@users.noreply.github.com>
2023-11-06 21:45:32 +0700

    Minh fix

--- Ecommerce/Controllers/CollectionsController.cs
+++ Ecommerce/Controllers/CollectionsController.cs
@@ -1,17 +1,29 @@
-﻿using System;
+﻿using Ecommerce.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
 
 namespace Ecommerce.Controllers
 {
     public class CollectionsController : Controller
     {
-        // GET: Collections
+        DoAnEntities database = new DoAnEntities();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Menu(string Searching)
+        {
+            if(Searching != null)
+            {
+                Session["search"]=Searching;
+            }
+            return View(database.Products.Where(x=>x.NamePro.Contains(Searching)||Searching==null).ToList());
+            
         }
     }
 }
